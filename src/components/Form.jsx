466d65e3118ae2f471d72f0ae717bfd12b8db713
Arnauw <arnaud.rabel@gmail.com>
2025-09-16 15:33:43 +0200

    input-text form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,8 +6,11 @@ class Form extends Component {
     state = {
         username: "",
     }
-    
-    
+
+    handleNickname = (e) => {
+        this.setState({ username: e.target.value })
+    }
+
     render() {
         return (
             <>
@@ -17,9 +20,11 @@ class Form extends Component {
                     <h1 className={"my-3"}>Commentary</h1>
 
                     <form action="" className="my-3">
-                        <label htmlFor="">Nickname</label>
-                        <input type="text" value={this.state.username}/>
+                        <label htmlFor="" className={"mr-4"}>Nickname</label>
+                        <input type="text" value={this.state.username} onChange={this.handleNickname} />
                     </form>
+
+                    <p>{this.state.username? `Your nickname is: ${this.state.username}`: null }</p>
                 </div>
 
             </>
