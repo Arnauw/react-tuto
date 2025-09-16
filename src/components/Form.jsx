bad0323346d78ea3eca0fef6b994bfb39652750e
Arnauw <arnaud.rabel@gmail.com>
2025-09-16 16:06:45 +0200

    color select input to finish

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,13 +2,27 @@ import {Component} from "react";
 import Car from "./Cars.jsx";
 
 class Form extends Component {
-    
+
     state = {
         username: "",
+        color: "",
+        colors: [
+            "",
+            "red",
+            "blue",
+            "green",
+            "black",
+            "purple",
+            "pink",
+        ],
     }
 
     handleNickname = (e) => {
-        this.setState({ username: e.target.value })
+        this.setState({username: e.target.value})
+    }
+
+    handleColor = (e) => {
+        this.setState({color: e.target.value})
     }
 
     render() {
@@ -18,13 +32,37 @@ class Form extends Component {
                 <div>
                     <Car color={"red"} height={"400"}></Car>
                     <h1 className={"my-3"}>Commentary</h1>
+                    <div>
+                        <form action="" className="my-3">
+                            <label htmlFor="" className={"mr-4"}>Nickname</label>
+                            <input type="text" value={this.state.username} onChange={this.handleNickname}/>
+                        </form>
 
-                    <form action="" className="my-3">
-                        <label htmlFor="" className={"mr-4"}>Nickname</label>
-                        <input type="text" value={this.state.username} onChange={this.handleNickname} />
-                    </form>
+                    </div>
 
-                    <p>{this.state.username? `Your nickname is: ${this.state.username}`: null }</p>
+                    <div className={"my-3"}>
+                        <label htmlFor="" className={"mr-4"}>Color</label>
+                        {/*<select name="" id="">*/}
+                        {/*    <option value="green">Green</option>*/}
+                        {/*    <option value="red">Red</option>*/}
+                        {/*    <option value="orange">Orange</option>*/}
+                        {/*</select>*/}
+                        <select name="" id="" onChange={this.handleColor}>
+                            {
+                                this.state.colors.map((color, index) => {
+                                    return <option key={index} value={color}>{color}</option>
+                                })
+                            }
+                        </select>
+                    </div>
+
+                    <p className={`my-3`}
+                       // style={{"color"}}
+                    >
+                        {
+                            this.state.username ? `Your nickname is: ${this.state.username}` : null
+                        }
+                    </p>
                 </div>
 
             </>
